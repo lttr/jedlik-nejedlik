d69a9df210e228aa3f2caaa5eb9e22944f8ce375
Lukas Trumm <lukas.trumm@gmail.com>
2023-10-23 07:39:41 +0200

    Install Sanity Nuxt module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  modules: ["@nuxtjs/sanity"],
   nitro: {
     preset: "netlify-edge",
+  },
+  sanity: {
+    projectId: "oppngufr",
+    dataset: "production",
   },
 });
