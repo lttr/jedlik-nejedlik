bb440800bcc9d9b4e1b287808f28cd54e8f86553
Lukas Trumm <lukas.trumm@gmail.com>
2024-07-25 08:25:27 +0200

    Temporary empty homepage

--- web/pages/index.vue
+++ web/pages/index.vue
@@ -1,25 +1,27 @@
 <template>
-  <div>
-    <Header class="p-page-layout" />
-    <main class="main p-page-layout p-stack">
-      <HeroSection />
-      <LecturesSection class="p-full-end" />
-      <IndividualSection />
-      <ExpertsSection />
-      <BlogSection />
-      <TestimonialsSection />
-      <AboutUsSection />
-      <FaqSection class="p-inset-padded" />
-      <ContactsSection />
-    </main>
-    <Footer class="p-page-layout p-full" />
-  </div>
+  <main>
+    <div class="p-stack p-center">
+      <div>
+        <SvgoLogoOneline class="logo" />
+      </div>
+      <div>
+        <NuxtLink to="/tabor">Tábor</NuxtLink>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script setup lang="ts"></script>
 
 <style scoped>
-.main {
-  --stack-space: var(--space-8);
+.logo {
+  width: 300px;
+  height: auto;
+}
+
+main {
+  height: 100vh;
+  display: grid;
+  place-content: center;
 }
 </style>
