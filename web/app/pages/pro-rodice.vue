0bff69f726062ee66348cafd2ddacbb35bd0b626
Lukas Trumm <lukas.trumm@gmail.com>
2025-02-27 21:17:46 +0100

    Add info about webinar

--- web/app/pages/pro-rodice.vue
+++ web/app/pages/pro-rodice.vue
@@ -1,13 +1,58 @@
 <template>
-  <PageWrapper>
-    <p class="p-center">
-      Stránky vznikají. Informace k individuálnímu poradenství najdete na
-      stránkách Zdeňky Trummové
-      <NuxtLink to="www.okolokuchyne.cz">www.okolokuchyne.cz</NuxtLink>
-    </p>
+  <PageWrapper root-element="article" class="p-prose">
+    <h1 class="p-base-text-bold">Webinář ke světovému dni obezity zdarma</h1>
+
+    <h2 class="p-heading-2">
+      Online poradna pro rodiče: Jak na nadváhu u dětí ve věku 4 - 8 let?
+    </h2>
+
+    <p class="p-base-text-bold">Termín webináře: 17. března 2025, 19:00</p>
+
+    <section>
+      <ul>
+        <li>Má vaše dítě vyšší tělesnou hmotnost?</li>
+        <li>Nevíte, jak začít změnu?</li>
+        <li>Máte obavy, aby se vaše dítě změnou netrápilo?</li>
+        <li>Nejste si jistí, zda je potřeba váhu vašeho dítěte řešit?</li>
+      </ul>
+
+      <p>
+        V rámci Světového dne obezity pořádáme <b>ZDARMA</b> online poradnu, ve
+        které budeme odpovídat na vaše dotazy k dětské nadváze a obezitě.
+      </p>
+
+      <p>
+        Dotazy posílejte mailem na
+        <NuxtLink href="mailto:info@jedlik-nejedlik.cz"
+          >info@jedlik-nejedlik.cz</NuxtLink
+        >
+        nebo prostřednictvím komentářů
+        <NuxtLink
+          href="https://fb.me/e/5kLg15ggj"
+          target="_blank"
+          rel="noopener"
+          >události na Facebooku.</NuxtLink
+        >
+      </p>
+
+      <p>
+        Nespoléhejte se, že vaše dítě z nadváhy vyroste. Čím dřív se nadměrná
+        hmotnost dítěte začne řešit, tím snadnější a úspěšnější je proces změny.
+      </p>
+
+      <!-- <p> -->
+      <!--   <b>Na událost je třeba se registrovat zadáním emailu níže.</b> -->
+      <!--   Na email vám bude před webinářem zaslán odkaz s přístupem. -->
+      <!--   <b>Všem přihlášeným bude zaslán záznam z webináře.</b> -->
+      <!-- </p> -->
+    </section>
   </PageWrapper>
 </template>
 
 <script lang="ts" setup></script>
 
-<style scoped></style>
+<style scoped>
+h2 {
+  margin-top: var(--space-2);
+}
+</style>
