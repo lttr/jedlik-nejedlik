f2a3e7771ac8b501ec73e526461468221dce4223
Lukas Trumm <lukas.trumm@gmail.com>
2025-05-29 12:46:22 +0200

    Change information on the page for parents

--- web/app/pages/pro-rodice.vue
+++ web/app/pages/pro-rodice.vue
@@ -1,58 +1,40 @@
 <template>
-  <PageWrapper root-element="article" class="p-prose">
-    <p class="p-center intro">
-      Stránky vznikají. Informace k individuálnímu poradenství najdete na
-      stránkách Zdeňky Trummové
-      <NuxtLink to="https://www.okolokuchyne.cz" class="p-heading-3"
-        >www.okolokuchyne.cz</NuxtLink
-      >
-    </p>
-
-    <h1 class="p-base-text-bold">Webinář ke světovému dni obezity zdarma</h1>
-
-    <h2 class="p-heading-2">
-      Online poradna pro rodiče: Jak na nadváhu u dětí ve věku 4 - 8 let?
-    </h2>
-
-    <p class="p-base-text-bold">Termín webináře: 17. března 2025, 19:00</p>
-
+  <PageWrapper class="p-flow">
     <section>
-      <ul>
-        <li>Má vaše dítě vyšší tělesnou hmotnost?</li>
-        <li>Nevíte, jak začít změnu?</li>
-        <li>Máte obavy, aby se vaše dítě změnou netrápilo?</li>
-        <li>Nejste si jistí, zda je potřeba váhu vašeho dítěte řešit?</li>
-      </ul>
-
-      <p>
-        V rámci Světového dne obezity pořádáme <b>ZDARMA</b> online poradnu, ve
-        které budeme odpovídat na vaše dotazy k dětské nadváze a obezitě.
-      </p>
-
-      <p>
-        Dotazy posílejte mailem na
-        <NuxtLink href="mailto:info@jedlik-nejedlik.cz"
-          >info@jedlik-nejedlik.cz</NuxtLink
-        >
-        nebo prostřednictvím komentářů
-        <NuxtLink
-          href="https://fb.me/e/5kLg15ggj"
-          target="_blank"
-          rel="noopener"
-          >události na Facebooku.</NuxtLink
+      <p class="p-center intro">
+        Stránky vznikají. Informace k individuálnímu poradenství najdete na
+        stránkách Zdeňky Trummové
+        <NuxtLink to="https://www.okolokuchyne.cz" class="p-heading-3"
+          >www.okolokuchyne.cz</NuxtLink
         >
       </p>
+    </section>
 
-      <p>
-        Nespoléhejte se, že vaše dítě z nadváhy vyroste. Čím dřív se nadměrná
-        hmotnost dítěte začne řešit, tím snadnější a úspěšnější je proces změny.
-      </p>
+    <section class="p-stack">
+      <h2>Kurzy a webináře</h2>
 
-      <!-- <p> -->
-      <!--   <b>Na událost je třeba se registrovat zadáním emailu níže.</b> -->
-      <!--   Na email vám bude před webinářem zaslán odkaz s přístupem. -->
-      <!--   <b>Všem přihlášeným bude zaslán záznam z webináře.</b> -->
-      <!-- </p> -->
+      <div class="p-stack">
+        <div class="p-cluster">
+          Webinář Jídlo na cesty pro děti a dospělé (odkaz na formulář)
+          <NuxtLink class="p-button p-button-brand" href="https://form.simpleshop.cz/xKQ4W/buy/">
+            Koupit
+          </NuxtLink>
+        </div>
+
+        <div class="p-cluster">
+          Webinář Co mám pořád vařit pro děti a dospělé (odkaz na formulář)
+          <NuxtLink class="p-button p-button-brand" href="https://form.simpleshop.cz/KmB6/buy/">
+            Koupit
+          </NuxtLink>
+        </div>
+
+        <div class="p-cluster">
+          Kurz zdravého (ne)hubnutí v Hradci Králové (odkaz na informace o kurzu)
+          <NuxtLink class="p-button p-button-brand" href="https://www.stob.cz/cs/kurz-zdraveho-ne-hubnuti-pro-deti-hradec-kralove-zakladni">
+            Koupit
+          </NuxtLink>
+        </div>
+      </div>
     </section>
   </PageWrapper>
 </template>
